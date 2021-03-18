9902d1492fad2baab4dff28496977534882db7bf
Lucas Adel <6014652@mborijnland.com>
2021-03-18 13:22:15 +0100

    begin gemaakt aan aanpassingen

---
+++ aanbiedingen.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="language" content="nl">
+  <meta name="viewport" content="width=device-width, intial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="L. Adel">
+  <meta name="keywords" content="">
+<link rel="stylesheet" type="text/css" href="css/main.css">
+  <title>Aanbiedingen</title>
+</head>
+  <body>
+    <header>
+        <article id="logo">
+            <img src="images/logo.png" alt="">
+        </article>
+        <nav id="navigatie">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Producten</a></li>
+                <li><a href="#">Evenementen</a></li>
+                <li><a href="#">Aanbiedingen</a></li>
+                <li><a href="#">Over Ons</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    <section class="a-main-tekst">
+      <h1>Week aanbiedingen</h1>
+      <p>Slideshow</p>
+    </section>
+    <section>
+      <h1>Aanbiedingen van volgende week</h1>
+      <p>Slideshow</p>
+    </section>
+    <footer>
+      <article class="item-footer">
+        <p class="copyright">2021 EKKO</p>
+        <p><a href="#">Contact</a></p>
+        <p><a href="#">Privacy</a></p>
+        <p><a href="#">FAQ</a></p>
+    </article>
+    </footer>
+
+  </body>
+</html>
