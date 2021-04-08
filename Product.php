9f0fced48e8d7ecba72537402b7dc586d7c45d4b
Bas Lepelaar <6010805@mborijnland.nl>
2021-04-08 10:57:40 +0200

    test

--- Product.php
+++ Product.php
@@ -60,29 +60,9 @@
 
 
     <main>
-    <section id="flex-product">
+    <section id>
 
-    <article>
-    <img class="Logo1" src="Images/EKKO_STRAWBEERRIES.png" alt="Ekko Strawberryt">
-    </article>
 
-    <article>
-    <img class="Logo2" src="Images/EKKO_ULTRACHOCO.png" alt="Ekko Choco">
-    </article>
-
-    <article>
-    <img class="Logo3" src="Images/EKKO_SUGAR_FREE.png" alt="Ekko sugar free">
-    </article>
-
-    <section class="flex-text">
-    <article>
-        <p><-- Hier links ziet u het logo voor 1 van onze producten. Wij maken meerdere energie drankjes waaronder de smaak EKKO STRAWBEERRIES. Onder strawberry ziet u nog 2 andere smaken. Als u geintresseerd bent in alle andere smaken dan kunt u de andere prodcuten vinden op (...)</p>
-    </article>
-    <section class="flex-maps">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2448.332937167458!2d4.490215115761184!3d52.14645467974523!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c5c663d413a42f%3A0xcaddc0f3c590aca3!2smboRijnland!5e0!3m2!1snl!2snl!4v1606395383923!5m2!1snl!2snl%22" allowfullscreen>
-        </iframe>
-    </section>
-    </section>
     </section>
       
 
